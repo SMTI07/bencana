b586deea3ef1cc7a5c6c2dacca809568819218ed
Alvian Rahman Hanif <alvian524@gmail.com>
2016-08-28 00:17:19 +0700

    setup environment utk kelompok query

--- Gemfile
+++ Gemfile
@@ -30,6 +30,16 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'jquery-turbolinks'
+gem 'bootstrap-sass'
+gem 'bootstrap-datepicker-rails'
+gem 'react-rails'
+gem 'will_paginate'
+gem 'cocoon'
+gem 'simple_form'
+gem 'fancybox2-rails'
+gem 'jasny-bootstrap-rails'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,12 +5,20 @@
 // or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
+// compiled file.
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require fancybox
+//= require jquery.turbolinks
+//= require bootstrap-sprockets
+//= require bootstrap-datepicker
+//= require react
+//= require react_ujs
+//= require cocoon
+//= require jasny-bootstrap.min
+//= require_tree ./react_components
 //= require_tree .
